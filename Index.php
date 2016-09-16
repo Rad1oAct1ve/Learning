a38ab68eea1154ffc1e0b4276a56a28358b68d3e
Rad1oAct1ve <121887117@qq.com>
2016-09-16 18:40:50 +0800

    test

---
+++ Index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: coolie
+ * Date: 2016/7/24
+ * Time: 15:44
+ */
+
+//$str = array();
+//$str[0] = 'LeBron';
+//$str[1] = 'Dwyane';
+//$str['w'] = 'James';
+//$str['a'] = 'Wade';
+//$str[3] = 'Anthony';
+//$str[2] = 'Carmelo';
+//print_r($str);
+
+include_once 'test.php';
+include 'test.php';
+
+sayFuckYou();
